aa2a5d051eb2958fd757a77f1a5a4413bb7e63a8
Sunny Kumar Singh <SRajukumarsingh@ivycomptech.com>
2024-10-08 20:14:48 +0530

    Playing with routes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.use('/',(req,res) => {
-    res.send('Hello from dashboard')
+
+app.get('/user/:userID', (req,res) => {
+    console.log(req.params)    //{ userID: '700' }
+    res.send({firstName:"Sunny",lastName:"Singh"})
 })
 
-app.use('/test',(req,res) => {
-    res.send('Hello from server test')
-})
+// app.post('/user', (req,res) => {
+//     res.send('User data is saved to DB.')
+// })
+
+// app.delete('/user', (req,res) => {
+//     res.send('User data deleted successfully')
+// })
+
+// app.put('/user', (req,res) => {
+//     res.send('User data updated')
+// })
+
+// app.use('/user', (req,res) => {
+//     res.send('Hello from user')
+// })
+
 
 app.listen(3000, () => {
     console.log('Server is successfully listening to port 3000');
